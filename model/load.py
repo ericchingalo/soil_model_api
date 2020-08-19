f872be08b74c3e4ebf27588ffeb2a7ba01940714
chingaloEric <echingalo@gmail.com>
2020-08-20 02:56:07 +0300

    feat: 🎸 added method to load saved model

---
+++ model/load.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import numpy as np
+import keras.models
+from keras.models import model_from_json
+import tensorflow as tf
+
+
+def init(): 
+	json_file = open('model.json','r')
+	loaded_model_json = json_file.read()
+	json_file.close()
+	loaded_model = tf.keras.models.model_from_json(loaded_model_json)
+
+	loaded_model.load_weights("model.h5")
+	print("Loaded Model from disk")
+
+	#compile and evaluate loaded model
+	loaded_model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+	
+	graph = tf.get_default_graph()
+
+	return loaded_model,graph
